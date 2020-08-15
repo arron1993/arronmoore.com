032fee23176b308b4db85f2db0881e84be7eb5e0
Arron Moore <arron.moore93@gmail.com>
2020-08-15 22:54:25 +0100

    Added start of projects, styling changes

--- arronmoore.com/src/components/header/index.js
+++ arronmoore.com/src/components/header/index.js
@@ -5,28 +5,29 @@ import Nav from 'react-bootstrap/Nav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+import "./header.scss";
 
 class Header extends React.Component {
     render() {
         return (
-            <Navbar style={{backgroundColor: "#000", zIndex: 99999}}>
+            <Navbar className="header sticky-top">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto text-white">
-                        <Nav.Link className="text-white" href="/">home</Nav.Link>
-                        <Nav.Link className="text-white" href="/about">about</Nav.Link>
-                        <Nav.Link className="text-white" href="/projects">projects</Nav.Link>
-                        <Nav.Link className="text-white" href="/photography">photography</Nav.Link>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">home</Nav.Link>
+                        <Nav.Link href="/about">about</Nav.Link>
+                        <Nav.Link href="/projects">projects</Nav.Link>
+                        <Nav.Link href="/photography">photography</Nav.Link>
                     </Nav>
                     <Nav className="d-none d-lg-flex">
                         <Nav.Link className="text-white" href="https://www.instagram.com/arron.moore93/">
-                            <FontAwesomeIcon icon={fab.faInstagram} />
+                            <FontAwesomeIcon size="lg" icon={fab.faInstagram} />
                         </Nav.Link>
                         <Nav.Link className="text-white" href="https://www.linkedin.com/in/arron-moore-269b8919b/">
-                            <FontAwesomeIcon icon={fab.faLinkedin} />
+                            <FontAwesomeIcon size="lg" icon={fab.faLinkedin} />
                         </Nav.Link>
                         <Nav.Link className="text-white" href="https://github.com/arron1993">
-                            <FontAwesomeIcon icon={fab.faGithub} />
+                            <FontAwesomeIcon size="lg" icon={fab.faGithub} />
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
--- arronmoore.com/src/pages/projects/index.js
+++ arronmoore.com/src/pages/projects/index.js
@@ -8,7 +8,31 @@ export default function Projects({data}) {
 
   return (
     <Layout pageName="projects">
-       
+       <div className="row mt-2">
+         <div class="col-12 row text-small">
+           <div className="col-12 text-center text-normal py-4"> This Website </div>
+           <div className="col-6">
+             Technologies
+             <ul>
+               <li>React</li>
+                <li>Gatsby</li>
+                <li>AWS</li>
+             </ul>
+           </div>
+           <div className="col-6">
+             <div>
+              Built myself as a way to teach myself the basics of React and the Gatsby framework.
+            </div>
+            <div className="pt-3">
+                Behind the scenes I used it as an opportunity to teach myself more about AWS. It is deployed using an AWS CodePipeline to
+                build a docker container that is deployed into ECS using a blue/green deployment.
+                Extremely over engineered for a site such as this.
+            </div>
+          </div>
+          
+           
+         </div>
+       </div>
     </Layout>
   )
 }
